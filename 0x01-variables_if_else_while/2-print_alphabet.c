9c6b7163daea9deac1b8279283ac33c7e37fa31f
Jihad Sheble <jihadsheble@gmail.com>
2023-03-17 13:39:22 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - a program that prints the alphabits
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+char c ;
+for (c = 'a';c <= 'z';c++)
+{
+putchar(c);
+putchar('\n');
+}
+return(0);
+}
+
