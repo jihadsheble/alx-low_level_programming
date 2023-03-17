ab6d89fe9977e319aaec885ea792b65bd085bde9
Jihad Sheble <jihadsheble@gmail.com>
2023-03-17 16:00:18 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - a program that prints all single digit numbers of base 10 starting from 0
+ * main - a program that prints all single digit numbers
  * Return: 0 (Success)
  */
 int main(void)
