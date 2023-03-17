6172c5acfa885b7746f61313a0d5e3ec73b2831c
Jihad Sheble <jihadsheble@gmail.com>
2023-03-16 17:50:57 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-
-int main(void) {
-  puts("Programming is like building a multilingual puzzle");
-  return (0);
+/**
+ * main - a program that prints puts function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
