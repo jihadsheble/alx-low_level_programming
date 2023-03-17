9454fd26bc4da6adb8d8a227893f9488b0434816
Jihad Sheble <jihadsheble@gmail.com>
2023-03-17 15:58:30 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - a program that prints all single digit numbers of base 10 starting from 0
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int c;
+for (c = '0'; c <= '9'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
