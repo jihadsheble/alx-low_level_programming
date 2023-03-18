e74d5c4a82c03eac5638e7759fb6eee4e4b904b5
Jihad Sheble <jihadsheble@gmail.com>
2023-03-17 17:17:41 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - a program that prints the alphabits
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+       int c;
+       char n;
+for (c = '0'; c <= '9'; c++)
+{
+putchar(c);
+}
+for (n = 'a'; n <= 'f'; n++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
