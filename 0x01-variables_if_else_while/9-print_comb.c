da0036b00570f3ac3296c9aa8630538e0519a7a9
Jihad Sheble <jihadsheble@gmail.com>
2023-03-17 17:37:55 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - a program that prints all possible combinations of numbers
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int c;
+for (c = '0'; c <= '9'; c++)
+{
+putchar(c);
+if (d != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
+
