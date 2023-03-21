75cd23dc234ca3995c849c0ce7b3508842acb4b0
Jihad Sheble <jihadsheble@gmail.com>
2023-03-21 16:46:45 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -a program that prints _putchar, followed by a new line. 
+ *
+ * Return: Always 0 (Success)
+ */
+int main ()
+{
+	printf("_putchar");
+	putchar('\n');
+	return (0);
+}
