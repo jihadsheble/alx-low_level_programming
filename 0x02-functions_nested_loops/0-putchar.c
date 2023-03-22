9ba11a7e63488cdc839c5d541833543bb586f635
Jihad Sheble <jihadsheble@gmail.com>
2023-03-21 17:24:37 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,21 @@
-#include <stdio.h>
+#include "main.h"
+	/**
+	 * main - print _putchar
+	 * is written by mbah
+	 * Return: 0
+	 */
 
-/**
- * main -a program that prints _putchar, followed by a new line. 
- *
- * Return: Always 0 (Success)
- */
-int main ()
-{
-	printf("_putchar");
-	putchar('\n');
+
+	int main(void)
+	{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
-}
+	}
