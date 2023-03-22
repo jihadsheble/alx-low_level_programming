6026566697d78118a5b713f0508cc9aaf26992e7
Jihad Sheble <jihadsheble@gmail.com>
2023-03-22 14:21:11 -0700

    0-main.c, main.h

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -7,17 +7,18 @@
 
 void positive_or_negative(int i)
 {
-if (i < 0)
-{
-printf("%d is negative", i);
-}
-else if (i == 0)
-{
-printf("%d is zero", i);
-}
-else if (i > 0)
-{
-printf("%d is positive", i);
-}
-printf("\n");
+	if (i < 0)
+	{
+		printf("%d is negative", i);
+	}
+	else if (i == 0)
+	{
+		printf("%d is zero", i);
+	}
+	else if (i > 0)
+	{
+		printf("%d is positive", i);
+	}
+
+	printf("\n");
 }
