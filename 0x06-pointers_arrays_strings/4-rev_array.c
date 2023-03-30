b04c2dd90e2345fc70d619f088e29592621cc833
Jihad Sheble <jihadsheble@gmail.com>
2023-03-30 03:45:15 -0700

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,15 +10,11 @@ void reverse_array(int *a, int n)
 {
 	int i;
 	int t;
-	int j;
-	j = sizeof(a);
 
-	for (i = 0; i < j; i++)
-{
+	for (i = 0; i < n--; i++)
+	{
 		t = a[i];
 		a[i] = a[n];
 		a[n] = t;
-		j--;
-
 	}
 }
