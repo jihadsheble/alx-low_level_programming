bfd350fdda335f083e108d298a91df58f16d2bae
Jihad Sheble <jihadsheble@gmail.com>
2023-04-10 17:10:54 -0700

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,7 +10,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int result, num_a , num_b;
+	int result, num_a, num_b;
 
 	if (argc < 3 || argc > 3)
 	{
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
 	}
 
 	num_a = atoi(argv[1]);
-	num_b= atoi(argv[2]);
+	num_b = atoi(argv[2]);
 	result = num_a * num_b;
 
 	printf("%d\n", result);
