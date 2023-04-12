322613272b9acc95b6db875ccc35ad34a8d3359b
Jihad Sheble <jihadsheble@gmail.com>
2023-04-11 18:19:17 -0700

    alloc grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -21,7 +21,7 @@ int **alloc_grid(int width, int height)
 	if (jj == NULL)
 		return (NULL);
 
-	for (i = 0; i< height; i++)
+	for (i = 0; i < height; i++)
 	{
 		jj[i] = malloc(sizeof(int) * width);
 
