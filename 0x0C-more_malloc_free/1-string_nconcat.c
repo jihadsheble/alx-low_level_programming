3fecfb56dad970f0e917d82c6aaa790adfa1e80d
Jihad Sheble <jihadsheble@gmail.com>
2023-04-14 16:42:42 -0700

    concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,6 +13,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *s;
 unsigned int x = 0, y = 0, len1 = 0, len2 = 0;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
 	while (s1 && s1[len1])
 		len1++;
