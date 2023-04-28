6a3823fc275e2237d85a6cc9c2202101ad7a77ac
jihadsheble <jihadsheble@gmail.com>
2023-04-27 19:02:52 -0700

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,15 +11,13 @@ size_t print_list(const list_t *h)
 {
 	size_t s = 0;
 
-	while (h)
-	{
-		if (!h->str)
-			printf("[0] (nil)\n");
-		else
-			printf("[%u] %s\n", h->len, h->str);
-		h = h->next;
-		s++;
-	}
+	for (s = 0; h; h = h->next, s++)
+{
+	if (!h->str)
+		printf("[0] (nil)\n");
+	else
+		printf("[%u] %s\n", h->len, h->str);
+}
 
 	return (s);
 }
