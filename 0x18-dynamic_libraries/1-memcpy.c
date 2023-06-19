51dc7dc948ac32ce7ea2d94365506babf672d279
jihadsheble <jihadsheble@gmail.com>
2023-06-19 14:28:13 -0700

    task done

---
+++ 0x18-dynamic_libraries/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy - function copies n bytes
+ * @dest: pointer to char params
+ * @src: pointer to char params
+ * @n: size
+ * Return: *dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+	{
+		dest[a] = src[a];
+	}
+	return (dest);
+}
